bd20d344a657b24016971496862be1c895bf635e
Daniel <Daniel@Daniel-PC>
2013-04-07 18:07:27 -0400

    Map Screen now has 2 buttons

--- OddJobs2/gen/com/oddjobs2/R.java
+++ OddJobs2/gen/com/oddjobs2/R.java
@@ -132,16 +132,19 @@ containing a value of this type.
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
+        public static final int button1=0x7f040007;
+        public static final int findjob=0x7f040008;
         public static final int hybrid=0x7f040004;
         public static final int map=0x7f040005;
-        public static final int menu_settings=0x7f040009;
+        public static final int menu_settings=0x7f04000c;
         public static final int none=0x7f040000;
         public static final int normal=0x7f040001;
-        public static final int password=0x7f040007;
+        public static final int password=0x7f04000a;
+        public static final int postjob=0x7f040006;
         public static final int satellite=0x7f040002;
-        public static final int signIn=0x7f040006;
+        public static final int signIn=0x7f040009;
         public static final int terrain=0x7f040003;
-        public static final int userName=0x7f040008;
+        public static final int userName=0x7f04000b;
     }
     public static final class layout {
         public static final int mapscreen=0x7f030000;
